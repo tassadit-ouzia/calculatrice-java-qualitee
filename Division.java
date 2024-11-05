347d6dfb5bc560e09f1a1c6a8d191eed1e8058f0
tassadit-ouzia <tassadit.ouzia04@gmail.com>
2024-11-05 12:34:34 +0100

    Update Division.java

--- Division.java
+++ Division.java
@@ -1,16 +1,41 @@
+/**
+ * Représente une opération de division entre deux expressions.
+ * 
+ * @author Ouzia Tassadit
+ * @version 2.0
+ */
 public class Division extends Operation {
+
+    /**
+     * Constructeur de la division.
+     * 
+     * @param operande1 Le numérateur de la division.
+     * @param operande2 Le dénominateur de la division.
+     */
     public Division(Expression operande1, Expression operande2) {
         super(operande1, operande2);
     }
 
+    /**
+     * Calcule la valeur de la division des deux opérandes.
+     * 
+     * @return Le résultat de la division.
+     * @throws ArithmeticException Si le dénominateur est égal à zéro.
+     */
     public double valeur() {
-    double denominateur = getOperande2().valeur();
-    if (denominateur == 0) {
-        throw new ArithmeticException("Division par zéro");
+        double denominateur = getOperande2().valeur();
+        if (denominateur == 0) {
+            throw new ArithmeticException("Division par zéro");
+        }
+        return getOperande1().valeur() / denominateur;
     }
-    return getOperande1().valeur() / denominateur;
-}
 
+    /**
+     * Retourne une représentation sous forme de chaîne de caractères de 
+     * l'opération de division.
+     * 
+     * @return La chaîne représentant la division.
+     */
     public String toString() {
         return "(" + getOperande1() + " / " + getOperande2() + ")";
     }

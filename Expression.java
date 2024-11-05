36023fa5e4a00a38a90082ef81c1849dd200dc0a
tassadit-ouzia <tassadit.ouzia04@gmail.com>
2024-11-05 12:33:27 +0100

    Update Expression.java

--- Expression.java
+++ Expression.java
@@ -1,3 +1,16 @@
+/**
+ * Classe abstraite représentant une expression.
+ * Les sous-classes doivent implémenter la méthode {@link #valeur()}.
+ * 
+ * @author Ouzia Tassadit
+ * @version 2.0
+ */
 public abstract class Expression {
-    public abstract int valeur();   
+
+    /**
+     * Méthode abstraite pour obtenir la valeur de l'expression.
+     * 
+     * @return La valeur de l'expression.
+     */
+    public abstract int valeur();
 }
